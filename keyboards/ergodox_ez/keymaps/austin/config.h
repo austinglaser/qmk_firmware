5d44f9f86389c2acd352412cc906fc7027c8bce1
Austin Glaser <austin@boulderes.com>
2018-07-24 19:56:15 -0600

    ergodox_ez/austin: Make quick rolling combos better

--- keyboards/ergodox_ez/keymaps/austin/config.h
+++ keyboards/ergodox_ez/keymaps/austin/config.h
@@ -4,3 +4,12 @@
 // Esc/Ctrl, it means that sometimes rapid keypresses don't register as
 // shortcuts, just as literal letters.
 #undef IGNORE_MOD_TAP_INTERRUPT
+
+// Makes quick rolling combos even better (for cases when a dual-function
+// modifier key is held down for a very short period of time).
+//
+// See docs for more details
+#define PERMISSIVE_HOLD
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 135
