7cc7deef34e3f28317f420c81d8a3a49362f81ce
Austin Glaser <austin@boulderes.com>
2019-11-22 12:37:57 -0700

    ergodox_ez/austin: Map right control

--- keyboards/ergodox_ez/keymaps/austin/keymap.c
+++ keyboards/ergodox_ez/keymaps/austin/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
                    KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
       KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
-                         KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,
+                         KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_RCTL,
 
       KC_TRNS, KC_TRNS,
       KC_TRNS,
