de1305b10fb9ebca8710f18aaba5f27634c0c04a
Austin Glaser <austin.glaser@gmail.com>
2022-03-14 18:38:50 -0700

    ergodox_ez/austin: add gaming layer
    The first change required here is to put spacebar on the left thumb, to
    support jumping with mouse + wasd. going forward, I suspect there will
    be other tweaks to make here.
    
    Would be nice to find a way to express this (at compile time) as a
    delta from my base keymap, rather than duping everything... but with the
    current way the LAYOUT macros work this doesn't look too feasible.

--- keyboards/ergodox_ez/keymaps/austin/keymap.c
+++ keyboards/ergodox_ez/keymaps/austin/keymap.c
@@ -9,13 +9,14 @@ enum custom_keycodes {
 enum layers {
   BASE = 0,
   FUNCTION,
+  GAMING,
   LAYER_COUNT
 };
 
 const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergodox(
-      KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  KC_TRNS,
+      KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(GAMING),
       KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  KC_TRNS,
       CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
       KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  KC_TRNS,
@@ -56,6 +57,28 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS,
       KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+  [GAMING] = LAYOUT_ergodox(
+      KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(BASE),
+      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  KC_TRNS,
+      CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
+      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  KC_TRNS,
+      KC_TRNS,          KC_TRNS, KC_TRNS,  KC_TRNS, MO(FUNCTION),
+
+                                                              KC_TRNS, KC_TRNS,
+                                                                       KC_TRNS,
+                                                  KC_SPACE, KC_BSPACE, KC_TRNS,
+
+      KC_6,        KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,  KC_EQUAL,
+      KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
+                   KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
+      KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
+                         KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_RCTL,
+
+      KC_TRNS, KC_TRNS,
+      KC_TRNS,
+      KC_TRNS, KC_ENTER, KC_SPACE
   ),
 
 };
