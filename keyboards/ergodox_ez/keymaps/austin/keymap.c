8cf4e2c4db5145aeb7e5e7030d54eedd3cda87b7
Austin Glaser <austin.glaser@gmail.com>
2022-03-14 18:43:30 -0700

    ergodox_ez/austin: use underscores instead of KC_TRNS

--- keyboards/ergodox_ez/keymaps/austin/keymap.c
+++ keyboards/ergodox_ez/keymaps/austin/keymap.c
@@ -17,68 +17,68 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergodox(
       KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(GAMING),
-      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  KC_TRNS,
+      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
       CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  KC_TRNS,
-      KC_TRNS,          KC_TRNS, KC_TRNS,  KC_TRNS, MO(FUNCTION),
+      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      _______,          _______, _______,  _______, MO(FUNCTION),
 
-                                                              KC_TRNS, KC_TRNS,
-                                                                       KC_TRNS,
-                                              GUI_T(KC_NO), KC_BSPACE, KC_TRNS,
+                                                              _______, _______,
+                                                                       _______,
+                                              GUI_T(KC_NO), KC_BSPACE, _______,
 
       KC_6,        KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,  KC_EQUAL,
       KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
                    KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
       KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
-                         KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_RCTL,
+                         _______, _______,  _______, _______,   KC_RCTL,
 
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_TRNS, KC_ENTER, KC_SPACE
+      _______, _______,
+      _______,
+      _______, KC_ENTER, KC_SPACE
   ),
 
   [FUNCTION] = LAYOUT_ergodox(
-      KC_TRNS, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,    KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+      _______, KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,    _______,
+      _______, _______, _______,  _______, _______, _______,  _______,
+      _______, _______, _______,  _______, _______, _______,
+      _______, _______, _______,  _______, _______, _______,  _______,
+      _______, _______, _______,  _______, _______,
 
-                                                    KC_TRNS,  KC_TRNS,
-                                                              KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,
+                                                    _______,  _______,
+                                                              _______,
+                                            _______, _______, _______,
 
       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-               KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+      _______, _______, _______, _______, _______,  _______, _______,
+               KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+      _______, _______, _______, _______, _______,  _______, _______,
+                        _______, _______, _______,  _______, _______,
 
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS
+      _______, _______,
+      _______,
+      _______, _______, _______
   ),
 
   [GAMING] = LAYOUT_ergodox(
       KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(BASE),
-      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  KC_TRNS,
+      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
       CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  KC_TRNS,
-      KC_TRNS,          KC_TRNS, KC_TRNS,  KC_TRNS, MO(FUNCTION),
+      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      _______,          _______, _______,  _______, MO(FUNCTION),
 
-                                                              KC_TRNS, KC_TRNS,
-                                                                       KC_TRNS,
-                                                  KC_SPACE, KC_BSPACE, KC_TRNS,
+                                                              _______, _______,
+                                                                       _______,
+                                                  KC_SPACE, KC_BSPACE, _______,
 
       KC_6,        KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,  KC_EQUAL,
       KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
                    KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
       KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
-                         KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_RCTL,
+                         _______, _______,  _______, _______,   KC_RCTL,
 
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_TRNS, KC_ENTER, KC_SPACE
+      _______, _______,
+      _______,
+      _______, KC_ENTER, KC_SPACE
   ),
 
 };
