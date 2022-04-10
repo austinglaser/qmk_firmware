e9597ae9b0f20b44ad625a0c4d427228c3fa0182
Austin Glaser <austin.glaser@gmail.com>
2022-04-10 16:35:30 -0700

    ergodox_ez/austin: get rid of the t

--- keyboards/ergodox_ez/keymaps/austin/keymap.c
+++ keyboards/ergodox_ez/keymaps/austin/keymap.c
@@ -39,11 +39,11 @@ enum layers {
 const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergodox(
-      KC_GRAVE,         KC_1,    KC_2,     KC_3,       KC_4,       KC_5,  TO(GAMING),
-      KC_TAB,           KC_Q,    KC_W,     KC_E,       KC_R,       KC_T,  _______,
-      CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,       KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,       KC_V,       KC_B,  _______,
-      _______,          _______, _______,  HYPR(KC_T), MO(GAMING_FUNCTION),
+      KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(GAMING),
+      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
+      CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
+      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      _______,          _______, _______,  KC_HYPR, MO(BASE_FUNCTION),
 
                                                               _______, _______,
                                                                        _______,
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE_FUNCTION] = LAYOUT_FUNCTION,
 
   [GAMING] = LAYOUT_ergodox(
-      KC_GRAVE,         KC_1,    KC_2,     KC_3,       KC_4,       KC_5,  TO(BASE),
-      KC_TAB,           KC_Q,    KC_W,     KC_E,       KC_R,       KC_T,  _______,
-      CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,       KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,       KC_V,       KC_B,  _______,
-      _______,          _______, _______,  HYPR(KC_T), MO(GAMING_FUNCTION),
+      KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(BASE),
+      KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
+      CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
+      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      _______,          _______, _______,  KC_HYPR, MO(GAMING_FUNCTION),
 
                                                               _______, _______,
                                                                        _______,
