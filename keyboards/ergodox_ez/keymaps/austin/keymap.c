64010fe6c4da628e5b458c217a26e6568de85bb5
Austin Glaser <austin.glaser@gmail.com>
2023-01-15 21:06:57 -0800

    ergodox_ez/austin: reconcile with upstream

--- keyboards/ergodox_ez/keymaps/austin/keymap.c
+++ keyboards/ergodox_ez/keymaps/austin/keymap.c
@@ -42,17 +42,17 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(GAMING),
       KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
       CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      KC_LEFT_SHIFT,    KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
       _______,          _______, _______,  KC_MEH,  MO(BASE_FUNCTION),
 
                                                               _______, _______,
                                                                        _______,
-                                              GUI_T(KC_NO), KC_BSPACE, _______,
+                                           GUI_T(KC_NO), KC_BACKSPACE, _______,
 
-      KC_6,        KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,  KC_EQUAL,
-      KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
-                   KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
-      KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
+      KC_6,             KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,     KC_EQUAL,
+      KC_LEFT_BRACKET,  KC_Y, KC_U,    KC_I,     KC_O,    KC_P,         KC_BACKSLASH,
+                        KC_H, KC_J,    KC_K,     KC_L,    KC_SEMICOLON, KC_QUOTE,
+      KC_RIGHT_BRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,     KC_RIGHT_SHIFT,
                          _______, _______,  _______, _______,   KC_RCTL,
 
       _______, _______,
@@ -66,17 +66,17 @@ const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRAVE,         KC_1,    KC_2,     KC_3,    KC_4,       KC_5,  TO(BASE),
       KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,       KC_T,  _______,
       CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,       KC_G,
-      KC_LSHIFT,        KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
+      KC_LEFT_SHIFT,    KC_Z,    KC_X,     KC_C,    KC_V,       KC_B,  _______,
       _______,          _______, _______,  KC_MEH,  MO(GAMING_FUNCTION),
 
                                                               _______, _______,
                                                                        _______,
-                                                  KC_SPACE, KC_BSPACE, _______,
+                                               KC_SPACE, KC_BACKSPACE, _______,
 
-      KC_6,        KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,  KC_EQUAL,
-      KC_LBRACKET, KC_Y, KC_U,    KC_I,     KC_O,    KC_P,      KC_BSLASH,
-                   KC_H, KC_J,    KC_K,     KC_L,    KC_SCOLON, KC_QUOTE,
-      KC_RBRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,  KC_RSHIFT,
+      KC_6,             KC_7, KC_8,    KC_9,     KC_0,    KC_MINUS,     KC_EQUAL,
+      KC_LEFT_BRACKET,  KC_Y, KC_U,    KC_I,     KC_O,    KC_P,         KC_BACKSLASH,
+                        KC_H, KC_J,    KC_K,     KC_L,    KC_SEMICOLON, KC_QUOTE,
+      KC_RIGHT_BRACKET, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,     KC_RIGHT_SHIFT,
                          _______, _______,  _______, _______,   KC_RCTL,
 
       _______, _______,
@@ -110,14 +110,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
-
-    uint8_t layer = biton32(state);
-
+layer_state_t layer_state_set_user(layer_state_t state) {
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+
+    uint8_t layer = get_highest_layer(state);
     switch (layer) {
       case 0:
         #ifdef RGBLIGHT_COLOR_LAYER_0
